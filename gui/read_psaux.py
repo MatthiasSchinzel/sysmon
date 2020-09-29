3c6bb1a9ed059c5565808203d61d1080456cb88e
Matthias Schinzel <mschinzel@gmx.de>
2020-09-29 18:48:39 +0200

    Adapting proto function

--- gui/read_psaux.py
+++ gui/read_psaux.py
@@ -1,9 +1,28 @@
 import subprocess
 
-ps = str(subprocess.Popen(['ps', 'aux', '--sort=-pcpu'], stdout=subprocess.PIPE).communicate()[0])
+ps = str(subprocess.Popen(['nvidia-smi', 'dmon', '-c', '1'], stdout=subprocess.PIPE).communicate()[0])
 processes = ps.split('\\n')
+processes.pop(0)
 processes.pop(0)
 processes.pop(-1)
 process = []
 for cur_process in processes:
-    process.append([j for j in cur_process.split(maxsplit = 10)])
+    process.append([j for j in cur_process.split(maxsplit = 9)])
+
+
+ps = str(subprocess.Popen(['command -v nvidia-sm'], stdout=subprocess.PIPE, shell=True).communicate()[0].decode("utf-8") )
+processes = ps.strip('\n')
+if not processes:
+    print('nvidia-smi not detected')
+
+ps = str(subprocess.Popen(['nvidia-smi', '-L'], stdout=subprocess.PIPE).communicate()[0].decode("utf-8"))
+processes = ps.split('\n')
+processes.pop(-1)
+id = 0
+gpu_num = len(processes)
+gpu_name = []
+for gpu in processes:
+    gpu = gpu..replace('GPU ' + str(id) + ': ', '')
+    gpu = gpu.split('(')
+    gpu_name.append(gpu[0].rstrip())
+    id += 1
